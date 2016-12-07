c8bdbde10c9d8bd3e760da0705098a8f9bf8e238
Jack Jou <jack8409201@gmail.com>
2016-12-08 02:45:09 +0800

    editting3.0

---
+++ editting3.0/assets/js/editting.js
@@ -0,0 +1,27 @@
+function flip_dialog(message, html) {
+
+    // fix backstyle
+    $('.flip-container .back').height($('.flip-container .front').height());
+
+    // scroll to top
+    $(window).scrollTop(0)
+
+    if(html)
+        $('.flip-container .back p').html(message);
+    else
+        $('.flip-container .back p').text(message);
+    
+    
+    // then flip that side
+    $('.flip-container').addClass('hover');
+    $('.flip-container .back button').click(function(){
+        $('.flip-container').removeClass('hover');
+    });
+    
+}
+
+$(function() {
+    $('#flip').click(function() {
+        flip_dialog("");
+    });
+});
